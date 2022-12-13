a9da0b9ff6ae5445e80b6cdf057d698c302e5fb2
Maksim Botalov <dev.botalov@gmail.com>
2022-12-13 14:01:33 +0500

    Initial Commit

---
+++ YourFinances/YourFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFinancesApp.swift
+//  YourFinances
+//
+//  Created by Максим Боталов on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct YourFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
